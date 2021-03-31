92c93a3f545717969131f21a3727da85e6222430
TimTheFiend <33222649+TimTheFiend@users.noreply.github.com>
2021-03-31 13:22:30 +0200

    Initial commit

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource efxSource;
+    public AudioSource musicSource;
+    public static SoundManager instance = null;  // Singleton
+
+    public float lowPitchRange = .95f;
+    public float highPitchRange = 1.05f;
+
+    void Awake()
+    {
+        if (instance == null) {
+            instance = this;
+        }
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySingle(AudioClip clip) {
+        efxSource.clip = clip;
+        efxSource.Play();
+    }
+
+    public void RandomizeSfx(params AudioClip[] clips) {  // `params´ Allows to do RandomizeSfx(AudioClip a, AudioClip b, AudioClip c, ...) 
+        int randomIndex = Random.Range(0, clips.Length);
+        float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+
+        efxSource.pitch = randomPitch;
+        efxSource.clip = clips[randomIndex];
+        efxSource.Play();
+    }
+
+
+}
